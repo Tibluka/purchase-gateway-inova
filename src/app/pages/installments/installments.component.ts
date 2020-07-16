ac9176735a174d56136c08d2c69e47fc7bda8cf3
Lucas Gomes <cprpc@hotmail.com>
2020-07-16 11:29:42 -0300

    Populando Formulário dos cartões

--- src/app/pages/installments/installments.component.ts
+++ src/app/pages/installments/installments.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+class formCardClass {
+  cardNumber: number;
+  cardName: string;
+  cardCPF: number;
+  cardCVC: number;
+  cardValidityPeriod: string;
+}
+
 @Component({
   selector: 'app-installments',
   templateUrl: './installments.component.html',
@@ -7,9 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstallmentsComponent implements OnInit {
 
+  qtdCard: number = 1
+  formCards: Array<formCardClass> = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formCards.push(new formCardClass())
+  }
+
+  cardQty() {
+    if (this.qtdCard < this.formCards.length) {
+      for (let index = 0; this.qtdCard < this.formCards.length; index++) {
+        this.formCards.pop()
+      }
+    } else {
+      for (let index = 0; this.formCards.length < this.qtdCard; index++) {
+        this.formCards.push(new formCardClass())
+      }
+    }
+  }
+
+  selectChangeHandler(event: any) {
+    this.qtdCard = event.target.value
+    this.cardQty()
+    console.log(this.formCards)
   }
 
 }
